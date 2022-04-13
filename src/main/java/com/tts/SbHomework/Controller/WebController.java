df36160d4ad572b3955c310ef670d8b14b1ebc34
pkranz27 <93950718+pkranz27@users.noreply.github.com>
2022-04-13 11:30:09 -0500

    Add files via upload

---
+++ src/main/java/com/tts/SbHomework/Controller/WebController.java
@@ -0,0 +1,32 @@
+package com.tts.SbHomework.Controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.tts.SbHomework.Form.PersonForm;
+@Controller
+public class WebController implements WebMvcConfigurer {
+	
+	public void addViewContollers (ViewControllerRegistry registry) {
+		registry.addViewController("/results").setViewName("results");
+	}
+	@GetMapping("/")
+	public String showForm(PersonForm personForm) {
+		return"form";
+	}
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "form";
+		}
+		return"redirect:/results";
+	}
+	
+	
+}
---
+++ src/main/java/com/tts/SbHomework/SpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.tts.SbHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeworkApplication.class, args);
+	}
+
+}
